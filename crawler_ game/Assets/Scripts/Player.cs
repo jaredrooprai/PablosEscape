ff707b2ec879d5e926111de87f8d22462db732be
KeiGiang <kevin.giang@hotmail.com>
2015-03-07 14:44:23 -0700

    minor changes to player class

--- crawler_ game/Assets/Scripts/Player.cs
+++ crawler_ game/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour {
 
 	public GameObject playerPrefab;
 	private GameObject instance;
-	private int[] playerLocation = {0,0};		// player's current location
+	private int[] playerLocation = {0,0};		// player's current location in (x,y) form
 	private int[] tempLocation = {0,0};			// used to store  player's previous location for sprite destruction
 
 	private int health;
@@ -51,7 +51,7 @@ public class Player : MonoBehaviour {
 	// Check to see if player-move input is valid
 	public void CheckMove (int mapMaxColumn, int mapMaxRow, int[] direction) {
 		if (direction[0] + playerLocation[0] <= mapMaxColumn && direction[1] + playerLocation[1] <= mapMaxRow) {
-			tempLocation[0] = playerLocation[0];
+			tempLocation[0]= playerLocation[0];
 			tempLocation[1] = playerLocation[1];
 			playerLocation[0] += direction[0];
 			playerLocation[1] += direction[1];
