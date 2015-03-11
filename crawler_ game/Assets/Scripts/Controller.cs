a990bd750560319a6f17d6b1e6d0c87cfb62ab90
jaredrooprai <roopraijared@gmail.com>
2015-03-10 23:32:37 -0600

    controller is now modular, player is handling linecasting

--- crawler_ game/Assets/Scripts/Controller.cs
+++ crawler_ game/Assets/Scripts/Controller.cs
@@ -5,21 +5,16 @@ public class Controller : MonoBehaviour {
 	
 	private Vector2 initTouchPos;
 	// current location
-	private float speed;
-	private Vector3 position; 
-	private Animator animator;
-	
-	
-	public void setVariables(float s, float rotationDegree, Vector3 currentPosition, Animator currentAnimator) {
-		transform.rotation = Quaternion.Euler (0, 0, rotationDegree);
-		speed = s;
-		position = currentPosition;          // Take the initial position
-		animator = currentAnimator;
+	private Vector3 position;
+	private float rotateDegree; // basically which way he should be facing
+
+	public float getRotation(){
+		return rotateDegree;
 	}
-	
-	
-	
-	public Vector3 touchScreen (){ // this is for the phone
+
+
+	public Vector3 touchScreen (Vector3 pos){ // this is for the phone
+		position = pos;
 		int fingercount = 0;
 		foreach (Touch touch in Input.touches) {
 			if (touch.phase == TouchPhase.Began){
@@ -28,68 +23,54 @@ public class Controller : MonoBehaviour {
 			
 			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled){
 				fingercount++;
-				
-				
+
 				if(fingercount ==1 && touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Ended){
 					Vector2 touchFacing = (initTouchPos - touch.position).normalized;
 					
-					if(Vector2.Dot (touchFacing, Vector2.up) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position )
-						position = checkMove (position, (position += Vector3.down), 0);
-					
-					if(Vector2.Dot (touchFacing, -Vector2.up) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position )
-						position = checkMove (position, (position += Vector3.up), 180);
-					
-					if(Vector2.Dot (touchFacing, Vector2.right) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position )
-						position = checkMove (position, (position += Vector3.left), 270);
-					
-					if(Vector2.Dot (touchFacing, -Vector2.right) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position )
-						position = checkMove (position, (position += Vector3.right), 90);
+					if(Vector2.Dot (touchFacing, Vector2.up) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position ){
+						position = position += Vector3.down;
+						rotateDegree = 0f;
+					}
+					else if(Vector2.Dot (touchFacing, -Vector2.up) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position ){
+						position = position += Vector3.up;
+						rotateDegree = 180f;
+					}
+					else if(Vector2.Dot (touchFacing, Vector2.right) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position ){
+						position = position += Vector3.left;
+						rotateDegree = 270f;
+					}
+					else if(Vector2.Dot (touchFacing, -Vector2.right) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position ){
+						position = position += Vector3.right;
+						rotateDegree = 90f;
+
+					}
 				}
 			}
 		}
-		return Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
+		return position;
 	}
-	
-	
-	
-	
-	public Vector3 keyboard(){  /// this is for the keyboard 
-		
-		if (Input.GetKey (KeyCode.A) && transform.position == position) 
-			position = checkMove (position, (position += Vector3.left), 270);
-		
-		else if (Input.GetKey (KeyCode.D) && transform.position == position)     
-			position = checkMove (position, (position += Vector3.right), 90);
-		
-		else if (Input.GetKey (KeyCode.W) && transform.position == position)         
-			position = checkMove (position, (position += Vector3.up), 180);
-		
-		else if (Input.GetKey (KeyCode.S) && transform.position == position)        
-			position = checkMove (position, (position += Vector3.down), 0);
-		
-		return Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
-	}
-	
-	
-	public Vector3 checkMove(Vector3 start, Vector3 end, float rotationDegree) {
-		transform.rotation = Quaternion.Euler (0, 0, rotationDegree);
 
-		// shows linecase for debugging purposes,
-		//Debug.DrawLine (start, end, Color.green);
-		bool wallCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("WallLayer"));
-		bool keyCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("KeyLayer"));
+	
+	public Vector3 keyboard(Vector3 pos){  /// this is for the keyboard 
+		position = pos;
 
-		
-		if (keyCollision == true) {
-			animator.SetTrigger ("Walk");
-			return end;
-		} else if (wallCollision == true) {
-			return start;
-		}else {
-			animator.SetTrigger("Walk");
-			return end;
+		if (Input.GetKey (KeyCode.A) && transform.position == position) {
+			position = position += Vector3.left;
+			rotateDegree = 270f;
+		} else if (Input.GetKey (KeyCode.D) && transform.position == position) {    
+			position = position += Vector3.right;
+			rotateDegree = 90f;
+		} else if (Input.GetKey (KeyCode.W) && transform.position == position) {        
+			position = position += Vector3.up;
+			rotateDegree = 180f;
+		} else if (Input.GetKey (KeyCode.S) && transform.position == position) { 
+			position = position += Vector3.down;
+			rotateDegree = 0f;
 		}
+
+		return position;
 	}
+	
 
 }
 
--- crawler_ game/Assets/Scripts/Player.cs
+++ crawler_ game/Assets/Scripts/Player.cs
@@ -3,29 +3,70 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	private float speed;
 	private Animator animator;
-
-	public Controller controllerScript;
+	private Controller controllerScript;
+	private Vector3 position;
+	private Vector3 direction;
 
 
 	void Start () {
 		animator = gameObject.GetComponent<Animator> ();
 		controllerScript = gameObject.GetComponent<Controller> ();
-		
-		controllerScript.setVariables (2f,180f, transform.position, animator); // setting speed and setting position
 
+		position = transform.position;
+		direction = transform.position;
+
+		transform.rotation = Quaternion.Euler (0, 0, 180f);
+		speed = 2f;
 	}
 
 	void Update (){
-
-
 	#if UNITY_STANDALONE || UNITY_EDITOR
-		transform.position = controllerScript.keyboard ();
+		direction = controllerScript.keyboard(position);
+		movement();
 	#else
-		transform.position = controllerScript.touchScreen();
+		direction = controllerScript.touchScreen(position);
+		movement();
 	#endif
-
 	}
+
+
+
+	private void movement(){
+		if ( position != (direction) )
+			position = checkMove (position, direction);
+		
+		transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
+	}
+
+
+
+
+	public Vector3 checkMove(Vector3 start, Vector3 end) {
+		transform.rotation = Quaternion.Euler (0, 0, controllerScript.getRotation());
+
+		//Debug.DrawLine (start, end, Color.green); // shows linecast for debugging purposes
+		
+		bool wallCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("WallLayer"));
+		bool keyCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("KeyLayer"));
+
+
+		if (keyCollision == true) {
+			return start;
+		}
+
+		if (wallCollision == true) {
+			return start;
+		}else {
+			animator.SetTrigger("Walk");
+			return end;
+		}
+	}
+
+
+
+
 
 }
 
