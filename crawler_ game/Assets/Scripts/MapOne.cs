b41ba76b30509a0c72562889588b0d687757ca87
jaredrooprai <roopraijared@gmail.com>
2015-03-12 21:08:40 -0600

    key drop shadows, box's now are items instead of floor

--- crawler_ game/Assets/Scripts/MapOne.cs
+++ crawler_ game/Assets/Scripts/MapOne.cs
@@ -38,19 +38,7 @@ public class MapOne : Map
 		{ 
 			for (int y = -1; y <= rows + 1; y++)
 			{	
-				if (x == 3 && y == 1)
-					tile = woodBox;
-				else if (x == 3 && y == 1)
-					tile = woodBox;
-				else if (x == 2 && y == 2)
-					tile = woodBox;
-				else if (x == 2 && y == 3)
-					tile = woodBox;
-				else if (x == 2 && y == 0)
-					tile = woodBox;
-				else if (x == 4 && y == 5)
-					tile = woodBox;
-
+			
 					// placing down a tile which represents the next room
 				/*
 				if (x == columns && y == rows)
@@ -58,7 +46,7 @@ public class MapOne : Map
 					*/
 				// depening on where the tile is placed it will either spawn a
 				// horizontal brick pattern or vertical brick battern
-				else if( x == -1 || x == columns + 1 || y == -1 || y == rows + 1){
+				if( x == -1 || x == columns + 1 || y == -1 || y == rows + 1){
 					tile = cementWall;
 				}
 				else 		// else spawn floor normal floor tile
@@ -72,6 +60,11 @@ public class MapOne : Map
 	// Method to add items into the map using parent spawnItem method
 	void setupItems(){
 		spawnPrefab (3, 3, keyPrefab);
+		spawnPrefab (2, 2, woodBox);
+		spawnPrefab (4, 4, woodBox);
+		spawnPrefab (3, 4, woodBox);
+		spawnPrefab (2, 5, woodBox);
+
 	}
 
 }
