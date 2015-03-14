f984124eab294f944fc37f2a18ba16ed80003bcd
jaredrooprai <roopraijared@gmail.com>
2015-03-14 00:59:47 -0600

    documentation in player class and code clean up

--- crawler_ game/Assets/Scripts/Player.cs
+++ crawler_ game/Assets/Scripts/Player.cs
@@ -5,38 +5,38 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
+	// different classes
 	private Animator animator;
 	private Controller controllerScript;
 	private PlayerHUD HUDScript;
-
+	
 	private Vector3 position;		// current position
 	private Vector3 newPosition;	// future position
 
-	private float speed;			// speed of movement, will be multiplied by time.delta time
-	private int health;
-	// booleans for colliding with objects
-	private bool wallCollision;		
-	private bool whiteGateCollision, hasWhiteKey;
-	private bool tealGateCollision, hasTealKey;
-	private bool goldGateCollision,	hasGoldKey;
+	private float speed;			// speed of movement from one vector 3 to another
+	private int health;				// Health point value
+
+	private bool wallCollision;				
+	private bool whiteGateCollision, tealGateCollision, goldGateCollision; 	// booleans for detecting if player is walking into a gate
+	private bool hasWhiteKey, hasTealKey, hasGoldKey;						// determining if player has a key to unlock gate or not
+	
 
 
 
-
-// initializing player 
+// initializing player attributes
 	void Start () {
 
 		animator = gameObject.GetComponent<Animator> ();
 		controllerScript = gameObject.GetComponent<Controller> ();
 		HUDScript = gameObject.GetComponent<PlayerHUD> ();
 
-		initHUD ();		// initialize health and key gui
-		rotatePlayer (); // rotating him on start up to face the right way 
+		initHUD ();													// initialize health and key gui
+		rotatePlayer (); 											// rotating him on start up to face the right way 
 
 	
-		position = transform.position;
-		newPosition = transform.position;
-		speed = 4f;
+		position = transform.position;			// getting current player position from players transform
+		newPosition = transform.position;		// newPosition and position need to be the same at the start in order make sure player can't move in between transitions...
+		speed = 4f;								// ... essentially newposition and position work hand in hand
 		health = 5;
 
 		hasWhiteKey = false;
@@ -44,7 +44,9 @@ public class Player : MonoBehaviour {
 		hasGoldKey = false;
 	}
 
-	private void initHUD (){ 		//initializeing Heads up display
+
+	// initializing players heads up display
+	private void initHUD (){ 		
 		HUDScript.toggleHeart_1(true);
 		HUDScript.toggleHeart_2(true);
 		HUDScript.toggleHeart_3(true);
@@ -57,109 +59,23 @@ public class Player : MonoBehaviour {
 
 
 
-	// Called every frame**********************************************************
+	// Called once every frame
 	void Update (){
-	// if game is running in the editor or on Mac Linux Pc. use keyboard controller
-	#if UNITY_STANDALONE || UNITY_EDITOR 
-		newPosition = controllerScript.keyboard(position); 		// get input from the keyboard fucntion in controller
-	// if game is running on android use touchscreen controller
-	# elif UNITY_ANDROID
-		newPosition = controllerScript.touchScreen(position); 	// get input from the touchscreen fucntion in controller
+		#if UNITY_STANDALONE || UNITY_EDITOR 					// if game is running in the editor or on Mac Linux Pc. use keyboard controller
+		newPosition = controllerScript.keyboard(position); 		// get players input and new position from keyboard controller
+
+		# elif UNITY_ANDROID									// if game is running on android use touchscreen controller
+		newPosition = controllerScript.touchScreen(position); 	//get players input and new position from touchscreen controller
 	#endif
-		movePlayer();// attempt to move player to correct grid position
-		checkHealth ();
-	}
 
-	
-
-	// move the player towards destination****************************
-	private void movePlayer(){
-		if (position != (newPosition)) {
-			position = checkCollider (position, newPosition);
-		}
-		transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);
-	}
-
-	
-	// rotating player to face direction of movement******************
-	private void rotatePlayer(){
-		if (((position.x) == (newPosition.x)) && ((position.y) == (newPosition.y))) {
-			transform.rotation = Quaternion.Euler (0, 0, 180f);
-		}else if ((position.x) > (newPosition.x)) {
-			transform.rotation = Quaternion.Euler (0, 0, 270f);
-		} else if ((position.x) < (newPosition.x)) {
-			transform.rotation = Quaternion.Euler (0, 0, 90f);
-		} else if ((position.y) < (newPosition.y)) {
-			transform.rotation = Quaternion.Euler (0, 0, 180f);
-		} else if ((position.y) > (newPosition.y)) {
-			transform.rotation = Quaternion.Euler (0, 0, 0f);
-		}
-	}
-	
-
-	
-	// check for player hitting colliders*****************************
-	private Vector3 checkCollider(Vector3 start, Vector3 end) {
-		rotatePlayer ();
-		//Debug.DrawLine (start, end, Color.green); // shows linecast for debugging purposes
-		
-		wallCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("WallLayer"));	// cast a line and check if its a wall
-		whiteGateCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("whiteGateLayer"));	// cast a line and check if its a key
-		tealGateCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("tealGateLayer"));	// cast a line and check if its a key
-		goldGateCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("goldGateLayer"));	// cast a line and check if its a key
-
-
-		if (whiteGateCollision == true && hasWhiteKey == false) { 
-			return start;
-		} else if (whiteGateCollision == true && hasWhiteKey == true) {
-			unlockWhiteGate ();
-			return end;
-
-		} else if (tealGateCollision == true && hasTealKey == false) {
-			return start;
-		} else if (tealGateCollision == true && hasTealKey == true) {
-			unlockTealGate ();
-			return end;
-
-		} else if (goldGateCollision == true && hasGoldKey == false) {
-			return start;
-		} else if (goldGateCollision == true && hasGoldKey == true) {
-			unlockGoldGate ();
-			return end;
-
-		} else if (wallCollision == true) {
-			return start;
-		} else {
-			animator.SetTrigger("Walk");
-			return end;
-		}
+		movePlayer();											// attempt to move player to correct grid position
+		checkHealth ();											// check if players health changed
 	}
 
 
 
-	void OnTriggerEnter2D (Collider2D other) {
-		if (other.tag == "WhiteKey") {
-			foundWhiteKey ();
-			Destroy (other.gameObject);
-		} else if (other.tag == "TealKey") {
-			foundTealKey ();
-			Destroy (other.gameObject);
 
-		} else if (other.tag == "GoldKey" ) {
-			foundGoldKey ();
-			Destroy (other.gameObject);
-
-		} else if (other.tag == "Milk") {
-			increaseHealth ();
-			Destroy (other.gameObject);
-		} else if (other.tag == "Trap") {
-			decreaseHealth(); 
-		}
-
-	}
-
-
-
+	//  updates health HUD in playerHUD class and makes sure that the health stays in bound. ( 0 <= health <= 5
 	private void checkHealth(){
 		if (health > 5) 
 			health = 5;
@@ -181,54 +97,110 @@ public class Player : MonoBehaviour {
 	}
 
 
-	private void increaseHealth(){
-		health += 1;
-	}
-
-
-	private void decreaseHealth(){
-		health -= 1;
-	}
 
 
 
-	//object interaction methods**************************************
-	private void foundWhiteKey(){
-		hasWhiteKey = true;
-		HUDScript.toggleKey_1(true);
-	}
-	private void foundTealKey(){	
-		hasTealKey = true;
-		HUDScript.toggleKey_2(true);
-	}
-	private void foundGoldKey(){	
-		hasGoldKey = true;
-		HUDScript.toggleKey_3(true);
+	// move the player towards destination
+	private void movePlayer(){
+		if (position != (newPosition)) {																// if player is not standing still
+			position = checkCollider (position, newPosition);											// check players future positino for colliders
+		}
+		transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime * speed);	// move the player to his new or old position
 	}
 
 
 
-	
-	private void unlockWhiteGate(){
-		HUDScript.toggleKey_1 (false);
-		animator.SetTrigger("Walk");
-		Destroy (GameObject.Find ("whiteGate(Clone)"));	
+
+
+	// rotating player to face direction of movement
+	private void rotatePlayer(){
+		if (((position.x) == (newPosition.x)) && ((position.y) == (newPosition.y))) {		// first if statement is really just there to make the player face upwards... 
+			transform.rotation = Quaternion.Euler (0, 0, 180f);								//... when the game starts
+		}else if ((position.x) > (newPosition.x)) {
+			transform.rotation = Quaternion.Euler (0, 0, 270f);								// rotating on z coord in terms of degrees
+		} else if ((position.x) < (newPosition.x)) {
+			transform.rotation = Quaternion.Euler (0, 0, 90f);
+		} else if ((position.y) < (newPosition.y)) {
+			transform.rotation = Quaternion.Euler (0, 0, 180f);
+		} else if ((position.y) > (newPosition.y)) {
+			transform.rotation = Quaternion.Euler (0, 0, 0f);
+		}
 	}
-	private void unlockTealGate(){
-		HUDScript.toggleKey_2 (false);
-		animator.SetTrigger("Walk");
-		Destroy (GameObject.Find ("tealGate(Clone)"));
+
+
+
+
+
+	// detecting for gates and walls is done with linecasting, but interactable items are done with triggers on the box colliders and their tags
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "WhiteKey") {
+			hasWhiteKey = true;
+			HUDScript.toggleKey_1(true);
+			Destroy (other.gameObject);
+		} else if (other.tag == "TealKey") {
+			hasTealKey = true;
+			HUDScript.toggleKey_2(true);;
+			Destroy (other.gameObject);
+
+		} else if (other.tag == "GoldKey" ) {
+			hasGoldKey = true;
+			HUDScript.toggleKey_3(true);
+			Destroy (other.gameObject);
+
+		} else if (other.tag == "Food") {
+			health += 1;
+			Destroy (other.gameObject);
+		} else if (other.tag == "Trap") {
+			health -= 1;
+		}
+
+	}
+
+
+
+
+
+	private Vector3 checkCollider(Vector3 start, Vector3 end) {
+		rotatePlayer ();																					
+		//Debug.DrawLine (start, end, Color.green); 														// shows linecast for debugging purposes
 		
+		wallCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("WallLayer"));			// cast a line and check if its a wall or gate..
+		whiteGateCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("whiteGateLayer"));// ..
+		tealGateCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("tealGateLayer"));	// ..
+		goldGateCollision = Physics2D.Linecast (start, end, 1 << LayerMask.NameToLayer ("goldGateLayer"));	// ..
+		
+		
+		if (whiteGateCollision == true && hasWhiteKey == false) { 
+			return start;
+		} else if (whiteGateCollision == true && hasWhiteKey == true) {
+			HUDScript.toggleKey_1 (false);
+			animator.SetTrigger("Walk");
+			Destroy (GameObject.Find ("whiteGate(Clone)"));
+			return end;
+			
+		} else if (tealGateCollision == true && hasTealKey == false) {
+			return start;
+		} else if (tealGateCollision == true && hasTealKey == true) {
+			HUDScript.toggleKey_2 (false);
+			animator.SetTrigger("Walk");
+			Destroy (GameObject.Find ("tealGate(Clone)"));
+			return end;
+			
+		} else if (goldGateCollision == true && hasGoldKey == false) {
+			return start;
+		} else if (goldGateCollision == true && hasGoldKey == true) {
+			HUDScript.toggleKey_3 (false);
+			animator.SetTrigger("Walk");
+			Destroy (GameObject.Find ("goldGate(Clone)"));
+			return end;
+			
+		} else if (wallCollision == true) {
+			return start;
+		} else {
+			animator.SetTrigger("Walk");
+			return end;
+		}
 	}
-	private void unlockGoldGate(){
-		HUDScript.toggleKey_3 (false);
-		animator.SetTrigger("Walk");
-		Destroy (GameObject.Find ("goldGate(Clone)"));
-	}
-	
-	
-
-                         
 
 
 
