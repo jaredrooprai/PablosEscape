eab37c4b760e6a438b5c8619647cc4a2791bf8fc
KeiGiang <kevin.giang@hotmail.com>
2015-03-16 17:40:35 -0600

    added Swipe and Hold for player movment

--- pablos_escape_game/Assets/Scripts/Controller.cs
+++ pablos_escape_game/Assets/Scripts/Controller.cs
@@ -16,16 +16,20 @@ public class Controller : MonoBehaviour {
 		position = pos;
 		int fingercount = 0;
 		foreach (Touch touch in Input.touches) {
+			// check if the finger is starting to touch the screen
 			if (touch.phase == TouchPhase.Began){
 				initTouchPos = touch.position;
 			}
-			
+
+			// if the finger has not lifted off the screen and the touch was registered properly
 			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled){
 				fingercount++;
 
-				if(fingercount ==1 && touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Ended){
+				// make sure there is only 1 finger, check if finger has moved on the screen or if the finger has swiped and stopped or if the finger is lifted
+				if(fingercount == 1 && touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Ended){
 					Vector2 touchFacing = (initTouchPos - touch.position).normalized;
-					
+
+					// determine movement vectors and set update variable appropriately 
 					if(Vector2.Dot (touchFacing, Vector2.up) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position ){
 						position+=Vector3.down;
 					} else if(Vector2.Dot (touchFacing, -Vector2.up) > 0.8 && Vector2.Distance (initTouchPos, touch.position) > 20 && transform.position == position ){
@@ -38,6 +42,7 @@ public class Controller : MonoBehaviour {
 				}
 			}
 		}
+		//return updates
 		return position;
 	}
 
