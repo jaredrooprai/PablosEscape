aefd11186089c6e36a7f046cff6942b9054f546c
jaredrooprai <roopraijared@gmail.com>
2015-03-16 19:53:53 -0600

    continue button only shows up when it should

---
+++ pablos_escape_game/Assets/Scripts/Main Menu/ContinueButtonHide.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueButtonHide : MonoBehaviour {
+	private GameObject ContinueButton;
+
+	// Use this for initialization
+	void Start () {
+		ContinueButton = GameObject.Find ("Continue");
+		if (PlayerPrefs.GetInt ("SavedLevel") == 1) {
+			ContinueButton.SetActive (false);
+		}
+		else{ContinueButton.SetActive(true);}
+
+	}
+
+	void update(){
+
+
+	}
+
+}
