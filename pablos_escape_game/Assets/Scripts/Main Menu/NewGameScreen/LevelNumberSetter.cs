2611a9b5a8fcf6f3638a99d1e98daadbdcbeda2d
jaredrooprai <roopraijared@gmail.com>
2015-03-16 21:18:05 -0600

    added level display on player dead button and added level display on new game scene

---
+++ pablos_escape_game/Assets/Scripts/Main Menu/NewGameScreen/LevelNumberSetter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelNumberSetter : MonoBehaviour {
+
+	private Text levelNumber;
+	// Use this for initialization
+	void Start () {
+		levelNumber = GameObject.Find ("Level Number").GetComponent<Text> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		levelNumber.text = "Level " + PlayerPrefs.GetInt("SavedLevel");
+
+	}
+}
---
+++ pablos_escape_game/Assets/Scripts/Main Menu/PlayerDead/RestartLevelText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RestartLevelText : MonoBehaviour {
+
+	private GameObject levelText;
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.Find ("Restart Level").GetComponentInChildren<Text>().text = "Restart Level " + PlayerPrefs.GetInt("SavedLevel");
+	
+	}
+}
