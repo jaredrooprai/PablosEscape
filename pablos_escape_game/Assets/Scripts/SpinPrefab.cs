25b4ac80215e7c3290a40eb218edd4a1cc95c12b
jaredrooprai <roopraijared@gmail.com>
2015-03-24 19:19:58 -0600

    removed key spinning and added portal spinnning

--- pablos_escape_game/Assets/Scripts/SpinPrefab.cs
+++ pablos_escape_game/Assets/Scripts/SpinPrefab.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spider : MonoBehaviour {
-
-	// Use this for initialization
-	public float rpm;
+public class SpinPrefab : MonoBehaviour {
 	
+	public float rpm;
+	//  public to set in prefab inspector
+
+
+
 	// Update is called once per frame
 	void Update () {
-
+		//rotate on z axis
 		transform.Rotate(0,0,6*rpm*Time.deltaTime);
-
+		
 	}
 }
