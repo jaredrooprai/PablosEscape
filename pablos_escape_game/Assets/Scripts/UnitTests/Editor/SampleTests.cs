39d09676be8c7db6170a5e981893d86674aeca7e
Jared Rooprai <roopraijared@gmail.com>
2015-03-31 22:26:40 -0600

    Added Directory for Unit Test scripts

---
+++ pablos_escape_game/Assets/Scripts/UnitTests/Editor/SampleTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+
+
+public class SampleTests{
+	[Test]
+	public void SimpleTest(){
+
+		Assert.That (2 + 2 == 4);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
